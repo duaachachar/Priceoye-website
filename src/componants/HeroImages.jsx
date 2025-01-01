785d0c14fed9bfb82ec51ad683cef2916395170d
duaachachar <duaabatoolchachar.xwave@gmail.com>
2025-01-01 09:23:19 +0000

    add icons in heroimages

--- src/componants/HeroImages.jsx
+++ src/componants/HeroImages.jsx
@@ -15,52 +15,70 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+// Material-UI Icons
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 // import required modules
 import { Autoplay, Navigation } from "swiper/modules";
 
 const HeroImages = () => {
   return (
-    <>
-      <Box className="my-8">
+    <div className="relative w-full">
+      {/* Hero Section */}
+      <Box className="my-8 relative">
         <Swiper
           slidesPerView={1}
           spaceBetween={30}
           autoplay={{
             enabled: true,
+            delay: 3000,
           }}
-          navigation={true}
+          navigation={{
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev",
+          }}
           modules={[Autoplay, Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <img src={FirstImage} alt="" />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <img src={SecImage} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={thirdImage} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={FourthImage} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={FifthImage} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={sixthImage} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={SevenImage} alt="" />
-          </SwiperSlide>
+          {/* Swiper Slides */}
+          {[
+            FirstImage,
+            SecImage,
+            thirdImage,
+            FourthImage,
+            FifthImage,
+            sixthImage,
+            SevenImage,
+          ].map((image, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={image}
+                alt={`Slide ${index + 1}`}
+                className="w-full object-cover rounded-lg"
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
+
+        {/* Custom Navigation Buttons */}
+        <div className="custom-prev bg-gray-600 absolute top-1/2 left-4 transform -translate-y-1/2 z-10 cursor-pointer text-white hover:text-gray-900">
+          <ArrowBackIcon className="text-4xl" />
+        </div>
+        <div className="custom-next bg-gray-600 absolute top-1/2 right-4 transform -translate-y-1/2 z-10 cursor-pointer text-white hover:text-gray-900">
+          <ArrowForwardIcon className="text-4xl" />
+        </div>
       </Box>
-      <Box>
-        <img src={SubHeroImage} alt="" />
+
+      {/* Sub Hero Section */}
+      <Box className="mt-8">
+        <img
+          src={SubHeroImage}
+          alt="Sub Hero"
+          className="w-full object-cover rounded-lg shadow-lg"
+        />
       </Box>
-    </>
+    </div>
   );
 };
 
